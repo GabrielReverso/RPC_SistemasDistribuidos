e85c95772f83fef36d57dd95f832ad415404a54b
GVerso <112283257+GabrielReverso@users.noreply.github.com>
2025-09-23 20:27:00 -0300

    RPC da calculadora

--- _web/src/main.ts
+++ _web/src/main.ts
@@ -1,11 +1,23 @@
-async function main() {
-  const response = await fetch("http://localhost:3000/add", {
-    method: "POST",
-    headers: { "Content-Type": "application/json" },
-    body: JSON.stringify({ a: 10, b: 32 }),
-  });
-  const data = await response.json();
-  console.log("Resultado da soma:", data.result);
+async function somar() {
+	const a_input = document.getElementById("a") as HTMLInputElement;
+	const b_input = document.getElementById("b") as HTMLInputElement;
+	const res = document.getElementById("res") as HTMLInputElement;
+	const a = parseInt(a_input.value);
+	const b = parseInt(b_input.value);
+
+	if (isNaN(a) || isNaN(b)) {
+		console.error("Por favor, insira números válidos.");
+		return;
+	}
+	const response = await fetch("http://localhost:3000/add", {
+		method: "POST",
+		headers: { "Content-Type": "application/json" },
+		body: JSON.stringify({ a, b }),
+	});
+	const data: { result: number } = await response.json();
+	console.log(data);
+	res.value = data.result.toString();
 }
 
-main();
+const button = document.querySelector("button") as HTMLButtonElement;
+button.addEventListener("click", somar);
