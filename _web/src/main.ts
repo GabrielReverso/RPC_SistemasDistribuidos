eab6045d002b313b92ff0ff10c058a2053260056
GVerso <112283257+GabrielReverso@users.noreply.github.com>
2025-09-25 21:52:06 -0300

    Alavanca adicionada

--- _web/src/main.ts
+++ _web/src/main.ts
@@ -1,14 +1,4 @@
 async function somar() {
-	/* const a_input = document.getElementById("a") as HTMLInputElement;
-	const b_input = document.getElementById("b") as HTMLInputElement;
-	const res = document.getElementById("res") as HTMLInputElement;
-	const a = parseInt(a_input.value);
-	const b = parseInt(b_input.value);
-
-	if (isNaN(a) || isNaN(b)) {
-		console.error("Por favor, insira números válidos.");
-		return;
-	} */
 	const response = await fetch("http://localhost:3000/spin", {
 		method: "POST",
 		headers: { "Content-Type": "application/json" },
@@ -20,3 +10,17 @@ async function somar() {
 
 const button = document.querySelector("button") as HTMLButtonElement;
 button.addEventListener("click", somar);
+
+const lever = document.getElementById("lever")!;
+const handle = document.getElementById("lever-handle")!;
+
+handle.addEventListener("click", () => {
+	lever.classList.add("pulled");
+	handle.classList.add("pulled");
+
+	// volta a posição original depois de 1s
+	setTimeout(() => {
+		lever.classList.remove("pulled");
+		handle.classList.remove("pulled");
+	}, 1000);
+});
